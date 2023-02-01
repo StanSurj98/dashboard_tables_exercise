e5dacdf5250026a4b93499942db2410404e413d9
Stanley Surjanto <stan.surjanto@gmail.com>
2023-02-01 16:47:18 -0700

    style: setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>E1</th>
+            <th>E2</th>
+            <th>E3</th>
+            <th>E4</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>R1</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <td>R2</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
