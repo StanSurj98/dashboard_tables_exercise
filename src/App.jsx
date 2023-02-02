268830a654372aef39933f8b2ce7e31a2c312ebd
Stanley Surjanto <stan.surjanto@gmail.com>
2023-02-02 16:39:26 -0700

    refactored state update for rows

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,45 @@ import "./App.css";
 import Row from "./components/Row";
 
 // Test Data
-const oneRowTest = [{ value: null }, { value: null }, { value: null }];
+// const oneRowTest = [{ value: null }, { value: null }, { value: null }];
+const NUM_OF_COLS = 7;
 
 function App() {
-  const [multiRowData, setMultiRowData] = useState([oneRowTest]);
+  const [multiRowData, setMultiRowData] = useState([
+    Array(NUM_OF_COLS).fill({ value: null }),
+  ]);
+  const [filledCellCount, setFilledCellCount] = useState(0);
+
+  // Handlers
+  const handleAddRow = (e) => {
+    e.preventDefault();
+    // console.log(e);
+    // Update state -> spread prev data and append a NEW array at the same # of cells as previous with default value: null
+    setMultiRowData((prev) => [
+      ...prev,
+      Array(prev[0].length).fill({ value: null }),
+    ]);
+    // console.log(multiRowData)
+  };
 
   return (
     <div className="App">
+      <p>Number of Cells Filled: {filledCellCount}</p>
+      <button type="submit" onClick={(e) => handleAddRow(e)}>
+        Add Row
+      </button>
       <table>
         <thead>
           <tr>
             <th></th>
+            {/* Ugly will do for now hahah */}
             <th scope="col">E1</th>
             <th scope="col">E2</th>
             <th scope="col">E3</th>
+            <th scope="col">E4</th>
+            <th scope="col">E5</th>
+            <th scope="col">E6</th>
+            <th scope="col">E7</th>
           </tr>
         </thead>
         <tbody>
@@ -26,12 +51,6 @@ function App() {
           ))}
         </tbody>
       </table>
-      <button
-        type="submit"
-        onClick={() => setMultiRowData((prev) => [...prev, oneRowTest])}
-      >
-        Add Row
-      </button>
     </div>
   );
 }
